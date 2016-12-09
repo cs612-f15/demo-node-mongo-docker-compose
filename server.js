a29dbdc95e11cab7fc5c179e9772f65a0233454a
Shawn Cicoria <cicorias@users.noreply.github.com>
2016-12-09 12:23:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+var MPORT=27017;
+
 var app = require('./app'),
     debug = require('debug')('demo:server'),
     util = require('util'),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 9000;
     MongoClient = require('mongodb').MongoClient,
-    mongoUrl = util.format('mongodb://mongo:%s/demo', process.env.MONGO_PORT);
-
+    mongoUrl = util.format('mongodb://mongo:%s/demo', MPORT);//process.env.MONGO_PORT);
+console.log(mongoUrl);
 MongoClient.connect(mongoUrl, function (err, db) {
   if (err) {
     console.error(err);
